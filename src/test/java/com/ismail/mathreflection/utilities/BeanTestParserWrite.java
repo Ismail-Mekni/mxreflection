c00ffd06c7f281c259f867358614d5a615f302af
Ismail MEKNI <ismail.mekni@sofrecom.com>
2020-09-18 23:03:54 +0100

    Full result parsing test cases

--- src/test/java/com/ismail/mathreflection/utilities/BeanTestParserWrite.java
+++ src/test/java/com/ismail/mathreflection/utilities/BeanTestParserWrite.java
@@ -3,6 +3,9 @@ package com.ismail.mathreflection.utilities;
 import com.ismail.mathreflection.annotations.MXFormula;
 import com.ismail.mathreflection.annotations.Variable;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 public class BeanTestParserWrite {
 
 
@@ -15,12 +18,39 @@ public class BeanTestParserWrite {
     @MXFormula("f1 - f2")
     public Integer field3;
 
-    @MXFormula("f1 + f2 + field3")
-    public String field4;
-
     @MXFormula("f1 * field4")
     public double field5;
 
     @MXFormula("sin(f2)")
     public Double field6;
+
+    @MXFormula("f1 - f2")
+    public float field7;
+
+    @MXFormula("f1 - f2")
+    public Float field8;
+
+    @MXFormula("f1 - f2")
+    public long field9;
+
+    @MXFormula("f1 - f2")
+    public Long field10;
+
+    @MXFormula("f1 - f2")
+    public short field11;
+
+    @MXFormula("f1 - f2")
+    public Short field12;
+
+    @MXFormula("f1 - f2")
+    public byte field13;
+
+    @MXFormula("f1 - f2")
+    public Byte field14;
+
+    @MXFormula("f1 - f2")
+    public BigInteger field15;
+
+    @MXFormula("f1 - f2")
+    public BigDecimal field16;
 }

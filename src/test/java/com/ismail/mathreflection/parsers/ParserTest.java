7bf7bd98714db60fc560cc0468421e05422a87ed
Ismail MEKNI <ismail.mekni@sofrecom.com>
2020-09-29 22:41:38 +0100

    Parser unit test class initialization

---
+++ src/test/java/com/ismail/mathreflection/parsers/ParserTest.java
@@ -0,0 +1,30 @@
+package com.ismail.mathreflection.parsers;
+
+import com.ismail.mathreflection.exceptions.UnparseableFieldException;
+import com.ismail.mathreflection.exceptions.UnparseableResultException;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ParserTest {
+
+    @Test
+    public void parseVariablesTest(){
+
+    }
+
+    @Test
+    public void parseResultTest(){
+
+    }
+
+    @Test(expected = UnparseableFieldException.class)
+    public void parseVariablesUnparseableFieldExceptionTest(){
+
+    }
+
+    @Test(expected = UnparseableResultException.class)
+    public void parseResultUnparseableResultExceptionTest(){
+
+    }
+}

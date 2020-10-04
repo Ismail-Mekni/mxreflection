bc7dd4b548faa0af541af58fbf6c594f81c352db
Ismail MEKNI <ismail.mekni@sofrecom.com>
2020-10-05 00:21:12 +0100

    Test removing Vertices without successors

--- src/test/java/com/ismail/mathreflection/utilities/GraphUtilityTest.java
+++ src/test/java/com/ismail/mathreflection/utilities/GraphUtilityTest.java
@@ -63,7 +63,65 @@ public class GraphUtilityTest {
     }
 
     @Test
-    public void removeVerticesWithoutSuccessors(){
+    public void getVerticesWithoutSuccessorFromCyclicGraphTest() {
+        Graph<String, DefaultEdge> graph = new DefaultDirectedGraph<>(DefaultEdge.class);
+        graph.addVertex("Vertex_1");
+        graph.addVertex("Vertex_2");
+        graph.addVertex("Vertex_3");
+        graph.addVertex("Vertex_4");
 
+        graph.addEdge("Vertex_1", "Vertex_2");
+        graph.addEdge("Vertex_1", "Vertex_3");
+        graph.addEdge("Vertex_2", "Vertex_4");
+        graph.addEdge("Vertex_3", "Vertex_4");
+        graph.addEdge("Vertex_4", "Vertex_1");
+
+        List<String> vertices = GraphUtility.getVerticesWithoutSuccessors(graph);
+
+        assertEquals(0, vertices.size());
+    }
+
+    @Test
+    public void removeVerticesWithoutSuccessorsTest(){
+        Graph<String, DefaultEdge> graph = new DefaultDirectedGraph<>(DefaultEdge.class);
+        graph.addVertex("Vertex_1");
+        graph.addVertex("Vertex_2");
+        graph.addVertex("Vertex_3");
+        graph.addVertex("Vertex_4");
+
+        graph.addEdge("Vertex_1", "Vertex_2");
+        graph.addEdge("Vertex_1", "Vertex_3");
+        graph.addEdge("Vertex_2", "Vertex_4");
+        graph.addEdge("Vertex_3", "Vertex_4");
+
+        GraphUtility.removeVerticesWithoutSuccessors(graph);
+
+        assertFalse(graph.containsVertex("Vertex_4"));
+        assertEquals(3, graph.vertexSet().size());
+
+        GraphUtility.removeVerticesWithoutSuccessors(graph);
+
+        assertFalse(graph.containsVertex("Vertex_3"));
+        assertFalse(graph.containsVertex("Vertex_2"));
+        assertEquals(1, graph.vertexSet().size());
+    }
+
+    @Test
+    public void removeVerticesWithoutSuccessorsFromCyclicGraphTest() {
+        Graph<String, DefaultEdge> graph = new DefaultDirectedGraph<>(DefaultEdge.class);
+        graph.addVertex("Vertex_1");
+        graph.addVertex("Vertex_2");
+        graph.addVertex("Vertex_3");
+        graph.addVertex("Vertex_4");
+
+        graph.addEdge("Vertex_1", "Vertex_2");
+        graph.addEdge("Vertex_1", "Vertex_3");
+        graph.addEdge("Vertex_2", "Vertex_4");
+        graph.addEdge("Vertex_3", "Vertex_4");
+        graph.addEdge("Vertex_4", "Vertex_1");
+
+        GraphUtility.removeVerticesWithoutSuccessors(graph);
+
+        assertEquals(4, graph.vertexSet().size());
     }
 }

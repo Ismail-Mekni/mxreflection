2640979831f8e465d0a9b3ad030b7a8d23e88dfc
Ismail MEKNI <ismail.mekni@sofrecom.com>
2020-10-06 23:43:42 +0100

    Initializing Reflection utility test

--- src/test/java/com/ismail/mathreflection/utilities/ReflectionUtilityTest.java
+++ src/test/java/com/ismail/mathreflection/utilities/ReflectionUtilityTest.java
@@ -1,4 +1,42 @@
 package com.ismail.mathreflection.utilities;
 
+import com.ismail.mathreflection.exceptions.DuplicatedVariableNameException;
+import org.junit.Test;
+
 public class ReflectionUtilityTest {
+
+    @Test
+    public void getClassFieldsTest(){
+
+    }
+
+    @Test
+    public void getClassFieldNamesTest(){
+
+    }
+
+    @Test(expected = DuplicatedVariableNameException.class)
+    public void getClassFieldNamesDuplicatedVariableNameExceptionTest(){
+
+    }
+
+    @Test
+    public void getFieldNameTest(){
+
+    }
+
+    @Test
+    public void getFieldValueTest(){
+
+    }
+
+    @Test(expected = NoSuchFieldException.class)
+    public void getFieldValueTestNoSuchFieldException(){
+
+    }
+
+    @Test
+    public void setValueToFieldTest(){
+
+    }
 }
